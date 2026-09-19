73e23c4c6ad22b7b7f5e885a066c1f7f8e55332a
unleaked <lzzxhq@gmail.com>
2026-09-19 03:24:22 -0700

    Update .lua

--- .lua
+++ .lua
@@ -2374,7 +2374,7 @@ end)
 local creditsSection = ataos:AddSection("Credits")
 creditsSection:AddParagraph("@lzzzx", "Made this plugin, if you have requests feel free to ask.")
 
-shared.Notify("ATAOs ON TOP NIGGA", 5)
+shared.Notify("ATAOs Successfully Loaded!", 1)
 
 RootMaid:GiveTask(function()
     
